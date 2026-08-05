24b2648c3e6e720bd8565c1733e03485a6afc59e
Naitsabes1955 <juansebasmosquera519@gmail.com>
2026-08-05 07:20:13 -0500

    "feat: add i18n configuration and query restrictions"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,21 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { queryClient } from '../src/shared/queryClient';
+
+// initialize i18n
+import '../src/shared/i18n';
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="dark" />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <StatusBar style="dark" />
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
